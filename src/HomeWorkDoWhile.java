8ac2d8883010670a89477dd67185208e3870b622
viktoria <viiikiii.g@gmail.com>
2022-10-26 22:18:05 +0200

    пробная попытка

--- src/HomeWorkDoWhile.java
+++ src/HomeWorkDoWhile.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 public class HomeWorkDoWhile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        //first level:
         int number;
         int division=2;
 
         do { number = scanner.nextInt();
-           if ( number % division == 0) {
+          if ( number % division == 0) {
                System.out.println( division);}
            } while (false);
 
+        //second level
         int x = scanner.nextInt();
-        int p = 20;
-        int K = scanner.nextInt();
+        int p = 10;
+        int k = scanner.nextInt();
 
-        do { x +
-
-
+        for (int i = 1; i <=k; i++) {
+            x += x * p /100;
+            System.out.println(x);
+        }
+        k = k * x;
+        System.out.println(k);
+        }
     }
-    }}
 
 
